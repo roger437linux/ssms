7838f94b6d93c6dfc9e1be02a2bf04902162fe62
roger437kinux <roger437linux@gmail.com>
2026-09-17 17:53:46 -0300

    Add content index

--- tar.sql
+++ tar.sql
@@ -11,3 +11,15 @@ insert into vendas
 (1000, 99.99);
 
 SELECT * FROM dbo.vendas;
+
+-- Criar índice
+CREATE INDEX idx_vendas_cliente
+ON vendas(id_cliente);
+
+-- Listar  índices de uma tabela
+SELECT t.name AS NomeTabela, i.name AS NomeIndice, i.type_desc AS TipoIndice
+FROM sys.tables t
+INNER JOIN sys.indexes i 
+ON t.object_id = i.object_id
+WHERE t.name = 'vendas'
+ORDER BY i.name;
